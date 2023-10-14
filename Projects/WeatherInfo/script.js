5ff12807dbaf7d2299e592153430324efa330172
kaushalkumar9 <rockstarkaushal323@gmail.com>
2023-10-15 02:33:55 +0530

    Modified the code according to the issue proposed

--- Projects/WeatherInfo/script.js
+++ Projects/WeatherInfo/script.js
@@ -1,58 +1,82 @@
-const API_key = "04f9904e8a20b69ff173a91dfefd6345";
-
-const userInput = document.getElementById("userInput");
-const getWeatherButton = document.getElementById("getWeatherButton");
-const weatherAttribute = document.getElementById("weatherAttribute");
-const displayResult = document.getElementById("displayResult");
-
-getWeatherButton.addEventListener("click", async function(){
-
-    const city_name = userInput.value;
-    const selectedAttribute = weatherAttribute.value;
-
-    if(city_name === "" || selectedAttribute === "none"){
-        displayResult.textContent = "Please enter a City & select an attribute.";
-        return ;
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = '04f9904e8a20b69ff173a91dfefd6345'; 
+  
+    const newDelhiTemp = document.getElementById('newDelhiTemp');
+    const bangaloreTemp = document.getElementById('bangaloreTemp');
+    const userInput = document.getElementById('userInput');
+    const getWeatherButton = document.getElementById('getWeatherButton');
+    const weatherAttribute = document.getElementById('weatherAttribute');
+    const displayResult = document.getElementById('displayResult');
+  
+    function fetchTemperature(cityName, elementToUpdate) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
+        .then((response) => response.json())
+        .then((data) => {
+          const temperature = data.main.temp;
+          elementToUpdate.textContent = `${temperature}°C`;
+        })
+        .catch((error) => {
+          elementToUpdate.textContent = 'Error fetching temperature';
+          console.error(error);
+        });
     }
-
-    try {
-
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}&units=metric`);
-
-        const data = await response.json();
-
-        let weatherInfo;
-
-        if(selectedAttribute === "temp"){
-            weatherInfo = data.main.temp;
+  
+    // Fetch temperature for New Delhi and Bangalore on page load
+    fetchTemperature('New Delhi', newDelhiTemp);
+    fetchTemperature('Bangalore', bangaloreTemp);
+  
+    getWeatherButton.addEventListener('click', async function () {
+        const city_name = userInput.value;
+        const selectedAttribute = weatherAttribute.value;
+    
+        if (city_name === '' || selectedAttribute === 'none') {
+          displayResult.textContent = 'Please enter a City & select an attribute.';
+          return;
         }
-        else if(selectedAttribute === "humidity"){
-            weatherInfo = data.main.humidity;
+    
+        try {
+          const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${apiKey}&units=metric`);
+          const data = await response.json();
+    
+          let weatherInfo;
+    
+          switch (selectedAttribute) {
+            case 'temp':
+              weatherInfo = data.main.temp;
+              break;
+            case 'humidity':
+              weatherInfo = data.main.humidity;
+              break;
+            case 'pressure':
+              weatherInfo = data.main.pressure;
+              break;
+            case 'feels_like':
+              weatherInfo = data.main.feels_like;
+              break;
+            case 'temp_min':
+              weatherInfo = data.main.temp_min;
+              break;
+            case 'temp_max':
+              weatherInfo = data.main.temp_max;
+              break;
+            case 'wind_speed':
+              weatherInfo = data.wind.speed;
+              break;
+            default:
+              weatherInfo = 'Unknown attribute';
+              break;
+          }
+    
+          displayResult.textContent = `Weather ${selectedAttribute} : ${weatherInfo}`;
+    
+        } catch (error) {
+          displayResult.textContent = 'Error while fetching data from API';
+          console.error(error);
         }
-        else if(selectedAttribute === "pressure"){
-            weatherInfo = data.main.pressure;
-        }
-        else if(selectedAttribute === "feels_like"){
-            weatherInfo = data.main.feels_like;
-        }
-        else if(selectedAttribute === "temp_min"){
-            weatherInfo = data.main.temp_min;
-        }
-        else if(selectedAttribute === "temp_max"){
-            weatherInfo = data.main.temp_max;
-        }
-        else if(selectedAttribute === "wind_speed"){
-            weatherInfo = data.wind.speed;
-        }
-
-        displayResult.textContent = `Weather ${selectedAttribute} : ${weatherInfo}`
-        
-    } catch (error) {
-        displayResult.textContent = "Error while fetching data from API";
-        console.log(error);
-    }
-})
-
-userInput.addEventListener("click" , ()=>{
-    userInput.value =""
-})
+    });
+  
+    userInput.addEventListener('click', () => {
+      userInput.value = '';
+    });
+  });
+  
